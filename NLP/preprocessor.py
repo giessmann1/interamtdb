61d4e0b7ca6c76d8e60a925561b18c2dc978d6f3
giessmann1 <giessmann1@uni-potsdam.de>
2023-11-20 20:26:35 +0100

    New processing structure

--- NLP/preprocessor.py
+++ NLP/preprocessor.py
@@ -9,6 +9,7 @@ sys.path.append('..')
 from database_wrapper import *
 # Run this before: python3 -m spacy download de_core_news_md
 
+nltk.download('stopwords')
 nltk.download('punkt')
 custom_stopwords = stopwords.words('german') + ['bzw']
 nlp = spacy.load('de_core_news_md', disable=['parser', 'ner'])
@@ -18,23 +19,23 @@ def lemmatize_word(word):
     return ''.join([token.lemma_ for token in nlp(word)]).lower()
 
 
+# Return a list with (type, processed_word)
 def process_word(word):
-    # Replace everything not text
-    word = re.sub(r'[^a-zA-ZäöüßÄÖÜ]', '', word)
-    # To lower case
-    word = word.lower()
-    # Stopword removal
-    word = '' if word in custom_stopwords else word
-    # Lemmatization, slower than stemming but more accurate
-    word = lemmatize_word(word)
-    if word == '':
-        return None
+    # Replace everything not text and lower case
+    processed_word = re.sub(r'[^a-zA-ZäöüßÄÖÜ]', '', word).lower()
+    if processed_word == '': return ('punctuation', None) # Return punctuations
 
-    return word
+    # Stopword removal
+    processed_word = '' if processed_word in custom_stopwords else processed_word
+    if processed_word == '': return ('stopword', None) # Return stopwords
+
+    # Lemmatization, slower than stemming but more accurate
+    processed_word = lemmatize_word(processed_word)
+    return ('word', processed_word)
 
 
 def interamt_preprocessor(interamt_col):
-    list_of_dicts = get_all_collection_docs(interamt_col, 10)
+    list_of_dicts = get_all_collection_docs(interamt_col, 1)
     # Pre-filter if not all columns are needed
     list_of_filtered_dicts = [
         {key: d.get(key) for key in ['ID', 'Stellenbeschreibung']} for d in list_of_dicts]
@@ -44,21 +45,22 @@ def interamt_preprocessor(interamt_col):
 
     df = pd.DataFrame(list_of_filtered_dicts)
 
-    # Tokenize sentences
+    # TODO: Somewhere a extra row is added
+    
+    # Tokenize sentences, this deletes nothing (maybe newlines?)
     df['sentence'] = df.apply(lambda row: sent_tokenize(
         row['Stellenbeschreibung'], 'german'), axis=1)
     df = df.explode('sentence')
 
-    # Tokenize words
+    # Tokenize words, this deletes nothing
     df['word'] = df.apply(lambda row: word_tokenize(
         row['sentence'], 'german'), axis=1)
     df = df.explode('word')
 
     # Preprocessing
-    df['word'] = df.apply(lambda row: process_word(row['word']), axis=1)
-    df = df.dropna(axis=0, subset=['word'])
+    df[['type', 'processed_word']] = df['word'].apply(process_word).to_list()
 
-    print(df['word'].value_counts())
+    print(df)
 
 
 if __name__ == '__main__':
